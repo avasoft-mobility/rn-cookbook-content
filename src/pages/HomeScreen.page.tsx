6065d51b665c63f63d52c83539e43dddf886bb4e
Ragul Raj Rajkumar <Ragul.RA@avasoft.com>
2022-07-15 19:02:32 +0530

    Remove unwantedLInes

--- src/pages/HomeScreen.page.tsx
+++ src/pages/HomeScreen.page.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
-interface HomeScreenProps {
-  navigation: any;
-}
-
-const HomeScreen: React.FC<HomeScreenProps> = props => {
+const HomeScreen: React.FC = props => {
   return (
     <View style={Styles.parentContainer}>
       <Text style={Styles.textContainer}>HomeScreen</Text>
