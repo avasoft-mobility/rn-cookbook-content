29e5364e9c8eb3586f307214a6433bf311efdc0e
Jiju  Selvakumar <Jiju.S@avasoft.com>
2022-07-18 22:10:55 +0530

    Pass data while pop a screen
    Pass the data when goback to previous page

--- src/pages/DetailScreen.Page.tsx
+++ src/pages/DetailScreen.Page.tsx
@@ -1,33 +1,31 @@
 import {RouteProp} from '@react-navigation/native';
 import React from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import {View, Text, StyleSheet, Button} from 'react-native';
 import {
   NavigationScreenProp,
   NavigationState,
   NavigationParams,
 } from 'react-navigation';
 
-interface HomeScreenProps {
+interface DetailScreenProps {
   navigation: NavigationScreenProp<NavigationState, NavigationParams>;
   route: RouteProp<{params: {name: string; age: number}}, 'params'>;
 }
 
-const HomeScreen: React.FC<HomeScreenProps> = props => {
+const DetailScreen: React.FC<DetailScreenProps> = props => {
   return (
     <View style={styles.parentContainer}>
-      <Text style={styles.text}>Home Screen</Text>
-      {props.route.params !== undefined ? (
-        <View style={styles.details}>
-          <Text style={styles.text}>Name: {props.route.params.name}</Text>
-          <Text style={styles.text}>Age: {props.route.params.age}</Text>
-        </View>
-      ) : null}
+      <Text style={styles.text}>Detail Screen</Text>
+      <View style={styles.details}>
+        <Text style={styles.text}>Name: {props.route.params.name}</Text>
+        <Text style={styles.text}>Age: {props.route.params.age}</Text>
+      </View>
       <Button
-        title="Submit"
+        title="Go Back"
         onPress={() =>
-          props.navigation.navigate('DetailScreen', {
-            name: 'John Carter',
-            age: 26,
+          props.navigation.navigate('HomeScreen', {
+            name: 'David beckham',
+            age: 36,
           })
         }
       />
@@ -50,4 +48,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default HomeScreen;
+export default DetailScreen;
