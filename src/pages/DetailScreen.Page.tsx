d93c4c30bdf8600a54d20b4f0020fae3e736c764
Jiju-avasoft <105510110+jiju-avasoft@users.noreply.github.com>
2022-07-18 18:40:11 +0530

    Implement goback navigation from DetailScreen to HomeScreen (#6)

--- src/pages/DetailScreen.Page.tsx
+++ src/pages/DetailScreen.Page.tsx
@@ -6,18 +6,15 @@ import {
   NavigationState,
 } from 'react-navigation';
 
-interface HomeScreenProps {
+interface DetailScreenProps {
   navigation: NavigationScreenProp<NavigationState, NavigationParams>;
 }
 
-const HomeScreen: React.FC<HomeScreenProps> = props => {
+const DetailScreen: React.FC<DetailScreenProps> = props => {
   return (
     <View style={styles.parentContainer}>
-      <Text style={styles.text}>Home Screen</Text>
-      <Button
-        title="Push to new screen"
-        onPress={() => props.navigation.navigate('DetailScreen')}
-      />
+      <Text style={styles.text}>Detail Screen</Text>
+      <Button title="Go back" onPress={() => props.navigation.goBack()} />
     </View>
   );
 };
@@ -34,4 +31,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default HomeScreen;
+export default DetailScreen;
