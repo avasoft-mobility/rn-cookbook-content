e691e5d9813f5ab445933efb36238b4b606f3791
Clement   Ambrose <Clement.A@avasoft.com>
2022-07-18 17:07:13 +0530

    Setup tab icon

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Image} from 'react-native';
 
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
@@ -11,7 +12,49 @@ const Tab = createBottomTabNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused}) => {
+            if (route.name === 'Home' && focused) {
+              return (
+                <Image
+                  style={{width: 40, height: 30}}
+                  source={require('./src/images/HomeIconFilled.png')}
+                />
+              );
+            }
+
+            if (route.name === 'Home' && !focused) {
+              return (
+                <Image
+                  style={{width: 40, height: 30}}
+                  source={require('./src/images/HomeIconOutlined.png')}
+                />
+              );
+            }
+
+            if (route.name === 'Settings' && focused) {
+              return (
+                <Image
+                  style={{width: 40, height: 30}}
+                  source={require('./src/images/SettingsIconFilled.png')}
+                />
+              );
+            }
+
+            if (route.name === 'Settings' && !focused) {
+              return (
+                <Image
+                  style={{width: 40, height: 30}}
+                  source={require('./src/images/SettingsIconOutlined.png')}
+                />
+              );
+            }
+            return;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}>
         <Tab.Screen
           name="Home"
           component={HomeScreen}
